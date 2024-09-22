885fc06133923dade8edd4fd103c399295f4184c
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-23 04:37:48 +0530

    Update ban.py

--- plugins/ban.py
+++ plugins/ban.py
@@ -33,7 +33,6 @@ warnsdb = mongodb.warns
 __MODULE__ = "Bᴀɴ"
 __HELP__ = """
 /ban - Ban A User
-/banall - Ban All Users
 /sban - Delete all messages of user that sended in group and ban the user
 /tban - Ban A User For Specific Time
 /unban - Unban A User
