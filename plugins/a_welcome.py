34ee02cb62f04ecc9efb47619fc54544d04d2bbb
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-23 04:33:15 +0530

    Update a_welcome.py

--- plugins/a_welcome.py
+++ plugins/a_welcome.py
@@ -18,11 +18,11 @@ SPAM_THRESHOLD = 2
 SPAM_WINDOW_SECONDS = 5
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://envs.sh/qNl.jpg",
+    "https://envs.sh/qNa.jpg",
+    "https://envs.sh/qNU.jpg",
+    "https://files.catbox.moe/5l1vtn.jpg",
+    "https://envs.sh/qNl.jpg",
 ]
 # --------------------------------------------------------------------------------- #
 
