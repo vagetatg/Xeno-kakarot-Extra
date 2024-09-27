b8468d91316055af615b5154490066c9cca8946b
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-27 08:56:24 +0530

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -2,7 +2,9 @@ import time, re
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
-
+from VIPMUSIC import app
+from VIPMUSIC.mongo.readable_time import get_readable_time
+from VIPMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
 async def active_afk(_, message: Message):
