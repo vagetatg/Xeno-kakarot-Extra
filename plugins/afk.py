8477eaf4a3c747038dfa22e6cbbaf4c11f3c4b3f
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-27 10:04:54 +0530

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -3,7 +3,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
 from mongo.readable_time import get_readable_time
-from mongo.afkdb import add_afk, is_afk, remove_afk
+from plugins.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
